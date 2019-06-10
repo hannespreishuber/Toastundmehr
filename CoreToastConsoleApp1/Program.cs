a9678cf516f8ecffc419385ff2e596b51237a0d5
Hannes Preishuber <hannesp@ppedv.de>
2019-06-10 10:07:26 +0200

    Toast und more

---
+++ CoreToastConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Notifications;
+
+namespace CoreToastConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+
+            var textNodes = template.GetElementsByTagName("text");
+            textNodes.Item(0).InnerText = "Hello worlds";
+
+            var notifier = ToastNotificationManager.CreateToastNotifier(@"{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe");
+            var notification = new ToastNotification(template);
+            notifier.Show(notification);
+
+        }
+    }
+}
--- Toastundmehr/MainPage.xaml.cs
+++ Toastundmehr/MainPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Xml.Dom;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +24,82 @@ namespace Toastundmehr
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        DispatcherTimer dp = new DispatcherTimer();
+
         public MainPage()
         {
             this.InitializeComponent();
+            dp.Interval = new TimeSpan(0, 0, 5);
+            dp.Tick += Dp_Tick;
+            dp.Start();
         }
+        private void Dp_Tick(object sender, object e)
+        {
+            string mytoast = $@"<toast>
+<visual branding='logo'>
+<binding template='ToastGeneric'>
+<text>Hallo Toast</text>
+<text>ich bins</text>
+<image src='Assets/Square44x44Logo.scale-200.png'/>
+<image src='Assets/Square44x44Logo.scale-200.png' placement='appLogoOverride' hint-crop='circle'/>
+</binding>
+</visual>
+</toast>
+";
+            var toastXml = new XmlDocument();
+            toastXml.LoadXml(mytoast);
+            var toast = new ToastNotification(toastXml);
+                toast.Failed += Toast_Failed;
+            toast.Activated += Toast_Activated;
+            toast.SuppressPopup = true;
+            //Windows.ApplicationModel.Package.Current.Id.Name
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+        private void Toast_Activated(ToastNotification sender, object args)
+        {
+        }
+
+        private void Toast_Failed(ToastNotification sender, ToastFailedEventArgs args)
+        {
+
+          }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string mytoast = $@"<toast>
+<visual branding='logo'>
+<binding template='ToastGeneric'>
+<text>Hallo Toast</text>
+<text>ich bins</text>
+<image src='Assets/Square44x44Logo.scale-200.png'/>
+<image src='Assets/Square44x44Logo.scale-200.png' placement='appLogoOverride' hint-crop='circle'/>
+</binding>
+</visual>
+</toast>
+";
+
+            //string title = "The current time is";
+            //string timeString = $"{DateTime.Now:HH:mm:ss}";
+            //string thomasImage = "https://www.thomasclaudiushuber.com/thomas.jpg";
+
+            //string toastXmlString =
+            //$@"<toast><visual>
+            //        <binding template='ToastGeneric'>
+            //        <text>{title}</text>
+            //        <text>{timeString}</text>
+            //        <image src='{thomasImage}'/>
+            //        </binding>
+       //         </visual></toast>";
+
+            var toastXml = new XmlDocument();
+            toastXml.LoadXml(mytoast);
+            var toast = new ToastNotification(toastXml);
+            toast.SuppressPopup = false;
+
+            var t = ToastNotificationManager.CreateToastNotifier();
+            t.Show(toast);
+        }
+
     }
 }
